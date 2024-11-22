77cf696b6fac234ff0478caab12afc761bdc6a4a
ngongocthang <ngongocthang18082004@gmail.com>
2024-11-22 09:56:17 +0700

    change

--- server/server.js
+++ server/server.js
@@ -14,13 +14,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: [
+      'https://loacalhost:5173',
+      'https://udemy-clone-6w54.vercel.app',
+      'https://udemy-clone-6w54-ku8zwly9z-ngongocthang123s-projects.vercel.app'
+    ]
   })
-);
+)
 
 app.use(express.json());
 
@@ -53,4 +62,4 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
-});
+}) 
